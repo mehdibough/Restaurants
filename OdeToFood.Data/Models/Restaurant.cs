7f130ff952580362f2b385b5d4a539be13f6c163
Mehdi boughambouz <mehdi.boughambouz@talan.com>
2022-06-03 04:23:23 +0200

    MyFirstRestaurantsASPNETRAZOR

---
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name="Type of food")]
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,53 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id =1, Name = "mehdiTacos", Cuisine = CuisineType.Arabic},
+                new Restaurant{Id =2, Name = "Chawarma", Cuisine = CuisineType.Italian},
+                new Restaurant{Id =3, Name = "Ratatouille", Cuisine = CuisineType.French}
+            };
+        }
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(r => r.Id) + 1;
+        }
+        public void Update(Restaurant restaurant)
+        {
+            var existing = Get(restaurant.Id);
+            if (existing != null)
+            {
+                existing.Name = restaurant.Name;
+                existing.Cuisine = restaurant.Cuisine;
+            }
+        }
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = Get(id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+        }
+    }
+}
+
